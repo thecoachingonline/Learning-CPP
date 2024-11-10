ddb3afd5e4b4c0ce013216890ad9e2c64a19b267
Y.Nanthachai <thecoachingonline@gmail.com>
2024-11-10 11:58:46 +0000

    Menu

---
+++ menu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "1: Print help " << std::endl;
+
+    std::cout << "2: Print exchange stats" << std::endl;
+
+    std::cout << "3: Make an offer " << std::endl;
+
+    std::cout << "4: Make a bid " << std::endl;
+
+    std::cout << "5: Print wallet " << std::endl;
+
+    std::cout << "6: Continue " << std::endl;
+    
+    return 0;
+}
