a936ce46da0e41fe943cd99984e87643a83b455b
Y.Nanthachai <thecoachingonline@gmail.com>
2024-11-30 02:18:26 +0000

    Float

--- test.cpp
+++ test.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+void printHello()
+{
+    std::cout << "Hello " << std::endl;
+}
+/** compute the average of x and y */
+float average(float x, float y)
+{
+    return (x+y) / 2;
+}
+
 int main()
 {
+    printHello();
+    float z = average(15, 7.5);
+    std::cout << z << std::endl;
+    
     std::cout << "Type in a number: " << std::endl;
 
     int x;
